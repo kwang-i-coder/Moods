c7bfab2a835c1eca92cd7dbe1f67ee123f6e310f
kwang-i-coder <wkk021121@gmail.com>
2025-07-29 00:31:38 +0900

    record_spaces 기능 추가

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ import "./lib/env.js"
 // 라우터 모듈 불러오기
 import auth_router from "./routers/auth.js";    
 import user_router from "./routers/user.js";
-
-
+import record_router from "./routers/record.js";
 
 const app = express();
 
@@ -16,13 +15,14 @@ app.use(express.static('public'))
 // post, patch에 대해 JSON 파싱 미들웨어 사용
 app.post("*splat",express.json());
 app.patch("*splat", express.json());
+
 // 라우터 설정
 app.use('/auth', auth_router);
 app.use('/user', user_router);
+app.use('/record', record_router);
 
 app.get("/", (req, res) => {
     res.send("this is root page");
 })
 
 app.listen(process.env.PORT)
-
