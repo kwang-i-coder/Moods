1d8a5d5ab167152784320b76fea50e393c41ef17
jdk829355 <jdk829355@gmail.com>
2025-08-04 15:00:11 +0900

    feat: 세션 종료, 취소, 조회기능 추가

--- routers/study-sessions.js
+++ routers/study-sessions.js
@@ -5,6 +5,7 @@ import verifySupabaseJWT from '../lib/verifyJWT.js'
 
 const router = express.Router()
 
+// 공부 세션 시작
 router.get('/start', verifySupabaseJWT, async (req, res) => {
     console.log('[라우트 호출] /study-sessions/start')
 
@@ -14,18 +15,21 @@ router.get('/start', verifySupabaseJWT, async (req, res) => {
     const start_time = new Date().toISOString();
     // 해당 유저의 세션이 저장되는 redis id
     const redis_key = `sessions:${req.user.sub}`
-
+    // redis_key에 해당하는 세션 데이터
     const session = await redisClient.hGetAll(redis_key);
 
+    // 세션이 이미 존재한다면 다중 세션 시도로 감지하고 에러 반환
     if (Object.keys(session).length !== 0) {
         console.log(`다중 세션 시도: ${redis_key}`)
         return res.status(400).send('이미 세션이 존재합니다.');
     }
 
+    // 공간 id가 누락된 경우
     if(!space_id){
         return res.status(400).send('space_id가 누락됐습니다.')
     }
     
+    // 레디스에 입력
     await redisClient.hSet(redis_key, {
         user_id: req.user.sub,
         space_id: space_id,
@@ -37,7 +41,10 @@ router.get('/start', verifySupabaseJWT, async (req, res) => {
     return res.status(200).json({success: true, start_time: start_time});
 })
 
+// 공부 세션 일시 정지
 router.get('/pause', verifySupabaseJWT, async (req, res) => {
+    console.log('[라우트 호출] /study-sessions/pause')
+
     const redis_key = `sessions:${req.user.sub}`;
     const session = await redisClient.hGetAll(redis_key);
 
@@ -45,6 +52,11 @@ router.get('/pause', verifySupabaseJWT, async (req, res) => {
         console.log(`세션 시작 안 함: ${redis_key}`);
         return res.status(400).send('세션이 없습니다.');
     };
+
+    if(session.status !== 'active'){
+        console.log(`중지 혹은 종료된 세션: ${redis_key}`);
+        return res.status(400).send(`session is ${session.status}`);
+    }
 
     const last_paused_at = new Date().toISOString();
     await redisClient.hSet(redis_key, {
@@ -56,7 +68,10 @@ router.get('/pause', verifySupabaseJWT, async (req, res) => {
 
 })
 
+// 공부 세션 재개
 router.get('/resume', verifySupabaseJWT, async (req, res) => {
+    console.log('[라우트 호출] /study-sessions/resume')
+
     const redis_key = `sessions:${req.user.sub}`;
     const session = await redisClient.hGetAll(redis_key);
 
@@ -68,14 +83,12 @@ router.get('/resume', verifySupabaseJWT, async (req, res) => {
 
     if (session.status !== 'paused') {
         console.log(`일시정지 상태가 아님: ${redis_key}`);
-        return res.status(400).send('일시정지 상태가 아닙니다.');
+        return res.status(400).send(`session is ${session.status}`);
     }
 
     const last_paused_at = new Date(session.last_paused_at);
-    console.log(`last_paused: ${last_paused_at}`)
     const resume_at = new Date();
     const duration = Math.floor((resume_at.getTime() - last_paused_at.getTime()) / 1000);
-    console.log("accumulated: "+ session.accumulatedPauseSeconds);
     const accumulatedPauseSeconds = Number(session.accumulatedPauseSeconds || 0) + duration;
 
     await redisClient.hSet(redis_key, {
@@ -90,5 +103,56 @@ router.get('/resume', verifySupabaseJWT, async (req, res) => {
     });
 });
 
+// 공부 세션 종료
+router.get('/finish', verifySupabaseJWT, async (req, res) => {
+    console.log('[라우트 호출] /study-sessions/finish')
+
+    const redis_key = `sessions:${req.user.sub}`;
+    const session = await redisClient.hGetAll(redis_key);
+
+    // 세션을 시작도 안했을 경우
+    if (Object.keys(session).length === 0) {
+        console.log(`세션 시작 안 함: ${redis_key}`);
+        return res.status(400).send('세션이 없습니다.');
+    };
+
+    // 이미 끝난 세션일 경우
+    if(session.status === 'finished'){
+        console.log(`종료된 세션에 대한 접근: ${redis_key}`);
+        return res.status(400).send('이미 세션이 종료됨');
+    };
+
+    const stopped_at = new Date();
+
+    // 일시정지된 세션에서 바로 종료하는 경우
+    if(session.status === 'paused'){
+        const last_paused_at = new Date(session.last_paused_at);
+        const duration = Math.floor((stopped_at.getTime() - last_paused_at.getTime())/1000);
+        const accumulatedPauseSeconds = Number(session.accumulatedPauseSeconds||0) + duration;
+        await redisClient.hSet(redis_key, {accumulatedPauseSeconds: accumulatedPauseSeconds});
+        console.log(`일시정지 상태에서 바로 종료: ${redis_key}`);
+    };
+
+    await redisClient.hSet(redis_key, {
+        status: 'finished',
+        end_time: stopped_at.toISOString()
+    });
+
+    console.log(`세션 종료됨: ${redis_key}`);
+    return res.status(200).json({success: true, end_time: stopped_at.toISOString()});
+});
+
+router.get('/user-session', verifySupabaseJWT, async (req, res) => {
+    const redis_key = `sessions:${req.user.sub}`;
+    const session = await redisClient.hGetAll(redis_key);
+
+    return res.status(200).json({success: true, data: session});
+});
+
+router.get('/quit', verifySupabaseJWT, async (req, res) => {
+    const redis_key = `sessions:${req.user.sub}`;
+    await redisClient.del(redis_key);
+    res.status(200).json({success:true})
+})
 
 export default router;
